76722b630df396a2a62f2f3602062c074cd763f2
Dima Lejkin <dmitrij.lejkin@abraxas.ch>
2023-03-10 20:56:04 +0100

    allvms deaktiviren auser dc01

--- apply__core.sh
+++ apply__core.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -e
 
 current_dir=$(pwd)
@@ -33,9 +33,9 @@ wait $thread3
 echo "thread3 (storage) exited with $?"
 
 # thread4 -> depends_on: thread2 && thread3
-echo "creating 04_wg"
-cd $build_path_terraform/04_wg && ./build.sh &
-thread4=$!
+#echo "creating 04_wg"
+#cd $build_path_terraform/04_wg && ./build.sh &
+#thread4=$!
 
 # thread5 -> depends_on: thread2 && thread3
 echo "creating 05_dc"
@@ -43,21 +43,14 @@ cd $build_path_terraform/05_dc && ./build.sh &
 thread5=$!
 
 # thread7 -> depends_on: thread2 && thread3
-echo "creating 07_rdg"
-cd $build_path_terraform/07_rdg && ./build.sh &
-thread7=$!
+#echo "creating 07_rdg"
+#cd $build_path_terraform/07_rdg && ./build.sh &
+#thread7=$!
 
 # thread8 -> depends_on: thread2 && thread3
-echo "creating 08_dbfs"
-cd $build_path_terraform/08_dbfs && ./build.sh &
-thread8=$!
-
-
-
-# thread88 -> depends_on: thread2 && thread3
-echo "creating 88_win_vm"
-cd $build_path_terraform/09_msrds && ./build.sh &
-thread88=$!
+#echo "creating 08_dbfs"
+#cd $build_path_terraform/08_dbfs && ./build.sh &
+#thread8=$!
 
 
 
@@ -82,9 +75,9 @@ echo "waiting for thread7 (rdg)..."
 wait $thread7
 echo "thread7 (rdg) exited with $?"
 
-#echo "waiting for thread8 (dbfs)..."
-#wait $thread8
-#echo "thread8 (dbfs) exited with $?"
+echo "waiting for thread8 (dbfs)..."
+wait $thread8
+echo "thread8 (dbfs) exited with $?"
 
 echo "waiting for thread9 (generic-dc)..."
 wait $thread9
@@ -96,37 +89,34 @@ cd $build_path_ansible \
 && ansible-playbook -i inventory infra-dc.yml &
 thread10=$!
 
-# thread11 ### zum test auskommentieren ###
+# thread11
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory infra-dh.yml \
 #&& ansible-playbook -i inventory infra-dh-containers.yml &
-#thread11=$! ### zum test auskommentieren ###
+#thread11=$!
 
 # thread13
-cd $build_path_ansible \
-&& ansible-playbook -i inventory generic-domain-members.yml &
-thread13=$!
+#cd $build_path_ansible \
+#&& ansible-playbook -i inventory generic-domain-members.yml &
+#thread13=$!
 
-echo "waiting for thread13 (generic-domain-members)..."
-wait $thread13
-echo "thread13 (generic-domain-members) exited with $?"
+#echo "waiting for thread13 (generic-domain-members)..."
+#wait $thread13
+#echo "thread13 (generic-domain-members) exited with $?"
 
-# thread14 ### zum test auskommentieren ###
+# thread14
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory generic-rdcb.yml \
 #&& ansible-playbook -i inventory generic-rdg.yml \
 #&& ansible-playbook -i inventory infra-mgmt-tools.yml &
 #thread14=$!
-### zum test auskommentieren ###
 
-# thread15 ### zum test auskommentieren ###
+# thread15
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory generic-fs.yml \
 #&& ansible-playbook -i inventory infra-fs.yml \
 #&& ansible-playbook -i inventory infra-sqldev.yml &
 #thread15=$!
-### zum test auskommentieren ###
-
 
 #echo "waiting for thread11 (infra-dh-containers)..."
 #wait $thread11
@@ -136,23 +126,19 @@ echo "thread13 (generic-domain-members) exited with $?"
 #wait $thread14
 #echo "thread14 (generic-rdg) exited with $?"
 
-# thread16 ### zum test auskommentieren ###
+# thread16
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory final-infra-dh-guac-connection-lists.yml &
 #thread16=$!
-### zum test auskommentieren ###
 
-
-# thread17 ### zum test auskommentieren ###
+# thread17
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory infra-rdg-certificates.yml &
 #thread17=$!
-### zum test auskommentieren ###
 
-
-echo "waiting for thread10 (infra-dc)..."
-wait $thread10
-echo "thread10 (infra-dc) exited with $?"
+#echo "waiting for thread10 (infra-dc)..."
+#wait $thread10
+#echo "thread10 (infra-dc) exited with $?"
 
 #echo "waiting for thread15 (generic-fs)..."
 #wait $thread15
@@ -168,35 +154,32 @@ echo "thread10 (infra-dc) exited with $?"
 #thread18=$!
 
 # thread19
-cd $build_path_ansible \
-&& ansible-playbook -i inventory infra-windows-international.yml &
-thread19=$!
+#cd $build_path_ansible \
+#&& ansible-playbook -i inventory infra-windows-international.yml &
+#thread19=$!
 
-# thread20 - ### zum test auskommentieren ###
+# thread20
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory final-windows-doc.yml &
 #thread20=$!
-### zum test auskommentieren ###
 
 #echo "waiting for thread16 (infra-dh-guac-connections)..."
 #wait $thread16
 #echo "thread16 (infra-dh-guac-connections) exited with $?"
 
-echo "waiting for thread19 (infra-windows-international)..."
-wait $thread19
-echo "thread19 (infra-windows-international) exited with $?"
+#echo "waiting for thread19 (infra-windows-international)..."
+#wait $thread19
+#echo "thread19 (infra-windows-international) exited with $?"
 
 #echo "waiting for thread20 (final-windows-doc)..."
 #wait $thread20
 #echo "thread20 (final-windows-doc) exited with $?"
 
-# thread21 - ### zum test auskommentieren ###
+# thread21
 #cd $build_path_ansible \
 #&& ansible-playbook -i inventory azure-network-dns-servers.yml \
 #&& ansible-playbook -i inventory generic-linux-reboot.yml &
 #thread21=$!
-### zum test auskommentieren ###
-
 
 #echo "waiting for thread18 (final-local-doc)..."
 #wait $thread18
